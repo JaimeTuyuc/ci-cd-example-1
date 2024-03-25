43b5f1ed5274756e15c9c231babcabfe4ab9aa56
JTuyuc7 <jaimetuyuc7@gmail.com>
2024-03-24 20:59:31 -0600

    feat: adding input

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState} from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+
+  const [name, setName] = useState("");
+  const handleNameChange = (inputText) => {
+    console.log(inputText, '*-*-*-*-*-')
+    //setName(inputText);
+  }
+  console.log(name, 'name')
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
 
-      <Text>My name is Jaime T</Text>
+      <Text>Hello User</Text>
+
+      <Button title="Click me" onPress={() => alert("Button Clicked")} />
+
+      <View >
+        <TextInput value={name} placeholder="Enter your name" onChangeText={setName} />
+        
+        <Text>Your name: {name}</Text>
+
+        <Button title="Send Name" onPress={() => alert(name)} />
+      </View>
     </View>
   );
 }
@@ -16,7 +34,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "purple",
     alignItems: "center",
     justifyContent: "center",
   },
